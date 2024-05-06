49317958013c4719fe4608885b8e615df2709212
Rocko <affontalvo@gmail.com>
2024-05-05 22:34:18 -0500

    feat: basic full functionality

---
+++ README.md
@@ -0,0 +1,12 @@
+# Download directory cleaner
+A CLI tool to deal with the mess that is my Downloads directory.
+I'm running it on WSL to handle the Windows downloads.
+
+## Usage
+Clone the repo.
+Rename .env_template to .env and write the corresponding paths to your case.
+Run clearer.py
+I make no assurances that it runs on your system.
+
+## Known issues
+Files with spaces in the name can't be moved (even after removing the spaces)
--- clearer.py
+++ clearer.py
@@ -5,31 +5,51 @@ book_exts = {'.epub', '.djvu', '.mobi'}
 img_exts = {'.jpg', '.jpeg', '.png', '.gif'}
 music_exts = {'.mp3', '.wma', '.ogg', '.wav'}
 video_exts = {'.mp4', '.wmv', '.mpeg'}
+install_exts = {'.exe', '.deb'}
 
 
-def get_dirs(test, env='.env'):
-    dirs = {
-        'downloads': None,
-        'library': None,
-        'img': None,
-        'music': None,
-        'video': None
-    }
+def get_dirs(test: bool, env='.env') -> dict[str, str]:
+    dirs = dict()
     with open(env, 'r') as dir_file:
         lines = dir_file.readlines()
         dirs = dict()
         for line in lines:
             if test:
-                lineformat = re.search('(.*)_DIR_TEST=(.*)', line)
+                lineformat = re.search('(.*)_DIR_TEST="(.*)"', line)
                 if lineformat:
                     dir_name, dir_path = lineformat.groups()
                     dirs[dir_name] = dir_path
             else:
-                lineformat = re.search('(.*)_DIR=(.*)', line)
+                lineformat = re.search('(.*)_DIR="(.*)"', line)
                 if lineformat:
                     dir_name, dir_path = lineformat.groups()
                     dirs[dir_name] = dir_path
     return dirs
+
+
+def pdfIsBook(file: str) -> bool:
+    book = input(f'is {file} a book?y/n')
+    isBook = True if book == 'y' else False
+    return isBook
+
+
+def manual_handle(file: str, dirs: dict[str, str]) -> None:
+    print(f'What do you want to do with {file}?')
+    opt = int(input("""
+        1. Delete
+        2. Move to Docs
+        3. Do nothing
+              """))
+    match opt:
+        case 1:
+            os.remove(file)
+        case 2:
+            new_file = ''.join(file.split())
+            os.rename(file, dirs['DOC']+'/'+new_file)
+        case 3:
+            print(f'{file} left unchanged. Moved on to next file')
+        case _:
+            print(f'Didn\'t understand that. Left {file} unchanged')
 
 
 def main(test=False):
@@ -39,16 +59,34 @@ def main(test=False):
     # loop over downfiles
     download_files = os.listdir()
     for file in download_files:
-        name, ext = os.path.splitext(file)
+        _, ext = os.path.splitext(file)
+        new_file=''.join(file.split())
         # delete installers
+        if ext in install_exts:
+            os.remove(file)
         # move images
+        elif ext in img_exts:
+            os.rename(file, dirs['IMG']+'/'+new_file)
         # move books
+        elif ext in book_exts:
+            os.rename(file, dirs['BOOK']+'/'+new_file)
         # move videos
+        elif ext in video_exts:
+            os.rename(file, dirs['VIDEO']+'/'+new_file)
         # move music
+        elif ext in music_exts:
+            os.rename(file, dirs['MUSIC']+'/'+new_file)
         # check if pdf is book
+        elif ext == '.pdf':
+            isBook = pdfIsBook(file)
         # move to books or docs accordingly
+            if isBook:
+                os.rename(file, dirs['BOOK']+'/'+new_file)
+            else:
+                os.rename(file, dirs['DOC']+'/'+new_file)
         # manually deal with other files
-    pass
+        else:
+            manual_handle(file, dirs)
 
 
 if __name__ == "__main__":
