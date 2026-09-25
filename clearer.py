eb872b394fed25cbc7a5730918d09c5a62857624
spargles <niklas.gassner@gmx.net>
2026-09-25 17:34:19 +0200

    Windows changes

--- clearer.py
+++ clearer.py
@@ -1,15 +1,25 @@
 import os
 import re
+import datetime
 from shutil import rmtree
+
+
+script_folder = os.path.dirname(os.path.abspath(__file__))
+log_file = os.path.join(script_folder, "logs.txt")
+
 
 book_exts = {'.epub', '.djvu', '.mobi', 'azw3'}
 img_exts = {'.jpg', '.jpeg', '.png', '.gif', '.PNG', '.JPEG'}
 music_exts = {'.mp3', '.wma', '.ogg', '.wav'}
-video_exts = {'.mp4', '.wmv', '.mpeg'}
+video_exts = {'.mp4', '.wmv', '.mpeg', '.mov'}
 install_exts = {'.exe', '.deb', 'rpm'}
+ignore_exts = {'.ini'} #windows download folder contains desktop.ini, which should be ignored
+
 
 
 def get_dirs(test: bool, env='.env') -> dict[str, str]:
+    #ensures .env file gets recognized if it's stored in the same place as python file
+    env = os.path.join(os.path.dirname(__file__), env) #can probably be adjusted via script_folder
     dirs = dict()
     with open(env, 'r') as dir_file:
         lines = dir_file.readlines()
@@ -29,9 +39,8 @@ def get_dirs(test: bool, env='.env') -> dict[str, str]:
 
 
 def pdfIsBook(file: str) -> bool:
-    book = input(f'is {file} a book?(y/n) ')
-    isBook = True if book == 'y' else False
-    return isBook
+    book = input(f'is {file} a book?(y/n) ').strip().lower()
+    return book == 'y'
 
 
 def manual_handle(file: str, dirs: dict[str, str]) -> None:
@@ -40,15 +49,18 @@ def manual_handle(file: str, dirs: dict[str, str]) -> None:
         "1. Delete\n"
         "2. Move to Docs\n"
         "3. Do nothing\n"
-    ))
+    ).strip())
     match opt:
         case 1:
             delete_path(file)
         case 2:
             new_file = ''.join(file.split())
             os.rename(file, dirs['DOC']+'/'+new_file)
+            write_log(file, 'DOC', log_file)
         case 3:
             print(f'{file} left unchanged. Moved on to next file')
+            with open(log_file, 'a') as logs:
+                logs.write(f"{file} left unchanged.\n")
         case _:
             print("Didn\'t understand that. Try again")
             manual_handle(file, dirs)
@@ -61,41 +73,63 @@ def delete_path(path: str) -> None:
         rmtree(path)
 
 
+def write_log(moved_file, directory, log_file_name):
+    with open(log_file_name, 'a') as logs:
+        logs.write(f"{moved_file} got moved to {directory}.\n")
+
+
 def main(test=False):
     # get directories for Download, Docs...
     dirs = get_dirs(test)
     os.chdir(dirs['DOWNLOAD'])
+    with open(log_file, 'a') as logs:
+        logs.write(f"===================================================\n")
+        logs.write(f"{datetime.datetime.now()}\n")
+        logs.write(f"===================================================\n")
     # loop over downfiles
     download_files = os.listdir()
     for file in download_files:
         _, ext = os.path.splitext(file)
         new_file = ''.join(file.split())
         # delete installers
-        if ext in install_exts:
+        if ext in ignore_exts:
+            with open(log_file, 'a') as logs:
+                logs.write(f"{file} ignored. No action taken.\n")
+        elif ext in install_exts:
             os.remove(file)
-        # move images
-        elif ext in img_exts:
-            os.rename(file, dirs['IMG']+'/'+new_file)
-        # move books
-        elif ext in book_exts:
-            os.rename(file, dirs['BOOK']+'/'+new_file)
-        # move videos
-        elif ext in video_exts:
-            os.rename(file, dirs['VIDEO']+'/'+new_file)
-        # move music
-        elif ext in music_exts:
-            os.rename(file, dirs['MUSIC']+'/'+new_file)
-        # check if pdf is book
-        elif ext == '.pdf':
-            isBook = pdfIsBook(file)
-        # move to books or docs accordingly
-            if isBook:
-                os.rename(file, dirs['BOOK']+'/'+new_file)
-            else:
-                os.rename(file, dirs['DOC']+'/'+new_file)
+            with open(log_file, 'a') as logs:
+                logs.write(f"{file} removed.\n")
+
         # manually deal with other files
-        else:
+        elif ext not in set(img_exts | book_exts | video_exts | music_exts) and ext != '.pdf':
             manual_handle(file, dirs)
+
+        else:
+            # move images
+            if ext in img_exts:
+                dir = 'IMG'
+            # move books
+            elif ext in book_exts:
+                dir = 'BOOK'
+            # move videos
+            elif ext in video_exts:
+                dir = 'VIDEO'
+            # move music
+            elif ext in music_exts:
+                dir = 'MUSIC'
+            # check if pdf is book
+            elif ext == '.pdf':
+                isBook = pdfIsBook(file)
+            # move to books or docs accordingly
+                if isBook:
+                    dir = 'BOOK'
+                else:
+                    dir = 'BOOK'
+
+            os.rename(file, dirs[dir]+'/'+new_file)
+            write_log(file, dirs[dir], log_file)
+    with open(log_file, 'a') as logs:
+        logs.write(f"===================================================\n")
 
 
 if __name__ == "__main__":
